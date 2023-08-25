d9c28cd247b0937fad7887e93c85426791602119
HakuAkai <102476175+HakuAkai@users.noreply.github.com>
2023-08-25 02:17:00 -0300

    Add files via upload

---
+++ marry.rb
@@ -0,0 +1,13 @@
+    puts "Do you, {full name}, wanna mary with me? "
+
+loop do 
+    res = gets.chomp
+    if res == "yes"
+        puts "I love u so honey!!! ^^"
+        101.times do |contador|
+            puts "I love you #{contador}%"
+        end
+        break
+        end
+        puts "Do you really think I will accept other thing different of yes?"
+    end
